178a8e9a71f3cf060bc1e863f6c8942119edc800
veerathp <veerathp@amazon.com>
2020-10-01 13:56:47 -0700

    First commit

--- etl/glue_etl.py
+++ etl/glue_etl.py
@@ -5,9 +5,10 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsglue.dynamicframe import DynamicFrame
+import boto3
 
 #Retrieve parameters for the Glue job.
-args = getResolvedOptions(sys.argv, ['JOB_NAME', 'S3_SOURCE', 'S3_DEST',
+args = getResolvedOptions(sys.argv, ['BUCKET', 'JOB_NAME', 'S3_SOURCE', 'S3_DEST',
                         'TRAIN_KEY', 'VAL_KEY'])
 
 sc = SparkContext()
@@ -15,6 +16,8 @@ glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
+
+logger = glueContext.get_logger()
 
 #Create a PySpark dataframe from the source table.
 source_data_frame = spark.read.load(args['S3_SOURCE'], format='csv',
@@ -24,11 +27,13 @@ source_data_frame = spark.read.load(args['S3_SOURCE'], format='csv',
 train_data, val_data = source_data_frame.randomSplit([.7,.3])
 
 #Write both dataframes to the destination datastore.
-train_path = args['S3_DEST'] + args['TRAIN_KEY']
+train_path = args['S3_DEST'] + args['TRAIN_KEY'] 
 val_path = args['S3_DEST'] + args['VAL_KEY']
 
 train_data.write.save(train_path, format='csv', mode='overwrite')
 val_data.write.save(val_path, format='csv', mode='overwrite')
+
+#logger.info("train_path" + train_path)
 
 #Complete the job.
 job.commit()
